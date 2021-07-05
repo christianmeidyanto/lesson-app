e41bf6d3f1d3a007aa3484917859bdd884f7bfd6
Christian Meidyanto <christianmeidyanto214@gmail.com>
2021-07-05 13:51:45 +0700

    Throw Error Done

--- src/Home.js
+++ src/Home.js
@@ -6,21 +6,32 @@ const Home = () => {
 
   const [isPending,setPending] = useState(true);
 
+  const [isErr,setErr]= useState(null);
+
   useEffect(()=>{
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
       .then(res=>{
+        if (!res.ok) {
+          throw Error('could not fetch the data for resource');
+        }
         return res.json();
       })
       .then(data=>{
         setBlogs(data);
-        setPending(false)
+        setPending(false);
+        setErr(null);
+      })
+      .catch(err =>{
+        setPending(false);
+        setErr(err.message);
       })
     }, 1000);
   },[]);
 
   return (
     <div className="home">
+      {isErr && <p>{isErr}</p>}
       {isPending && <p>Loading.....</p>}
       {blogs && <BlogList blogs={blogs} title={"All Blogs!"}/>}
     </div>
